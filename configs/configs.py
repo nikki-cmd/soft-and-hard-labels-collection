c07fd4861f71e9691a4e94d5e6e5bdace3bd8f15
Nick <kukuskinnikita72@gmail.com>
2025-06-23 22:32:47 +0300

    Small fixes

--- configs/configs.py
+++ configs/configs.py
@@ -2,6 +2,8 @@ from llama_cpp import Llama
 from huggingface_hub import hf_hub_download
 
 dataset_path = "datasets/simple_tasks_dataset.csv"
+answered_dataset_path = "datasets/answered_dataset.csv"
+distribution_matrix = "datasets/softlabels_numpy_zip.npz"
 
 model_path = hf_hub_download(
     repo_id="TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF",
@@ -11,7 +13,7 @@ model_path = hf_hub_download(
 llm = Llama(
     model_path=model_path,
     n_ctx=512,
-    logits_all=True,  # Это критически важно
+    logits_all=True,
     verbose=False
 )
 
--- data_tools/uploader.py
+++ data_tools/uploader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import configs.configs as cfg
 
 class Uploader():
     def __init__(self, path, answers, distributions_matrix):
@@ -10,9 +11,9 @@ class Uploader():
     def upload_answers(self):
         df = pd.read_csv(self.data_path)
         df['llama_attempt'] = self.answers
-        df.to_csv("new_dataset.csv", index=False)
+        df.to_csv(cfg.answered_dataset_path, index=False)
         
     def upload_distributions(self):
         print(len(self.distributions_matrix))
-        np.savez("softlabels_numpy_zip.npz", *self.distributions_matrix)
+        np.savez(cfg.distribution_matrix, *self.distributions_matrix)
         
