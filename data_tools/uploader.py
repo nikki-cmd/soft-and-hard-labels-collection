10c31fa48612af7c68265d1041f1b5bb79e9b630
Nick <kukuskinnikita72@gmail.com>
2025-06-29 14:50:27 +0300

    Add logging

--- data_tools/uploader.py
+++ data_tools/uploader.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import configs.configs as cfg
+from datetime import datetime
+import os
 
 class AnswersUploader():
     def __init__(self, path, answers):
@@ -14,11 +16,16 @@ class AnswersUploader():
 
     
 class SoftsUploader():
-    def __init__(self, distributions_matrix, question_id):
+    def __init__(self, distributions_matrix, question_id, current_time):
         self.distributions_matrix = distributions_matrix
         self.question_id = question_id
+        self.current_time = current_time
             
     def upload_distributions(self):
-        np.savez(f'softlabels/question_{self.question_id}.npz', *self.distributions_matrix)
+        
+        folder_path = f'softlabels/run{self.current_time}'
+        os.makedirs(folder_path, exist_ok=True)
+        
+        np.savez(os.path.join(folder_path, f'question_{self.question_id}.npz'),*self.distributions_matrix)
     
         
--- main.py
+++ main.py
@@ -2,6 +2,22 @@ from configs import configs
 from data_tools.dataloader import Loader 
 from data_tools.uploader import SoftsUploader, AnswersUploader
 from tools.get_soft_labels import getSL 
+import logging
+from datetime import datetime
+import time
+import sys
+
+current_time = datetime.now().strftime("%Y_%m_%d_%H_%M")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(f"logs/run_{current_time}.log", encoding='utf-8'),
+        logging.StreamHandler(stream=open(sys.stdout.fileno(), 'w', encoding='utf-8', closefd=False))
+    ]
+)
+logger = logging.getLogger(__name__)
 
 loader = Loader(path=configs.dataset_path)
 loaded_dataset = loader.get_questions()
@@ -12,18 +28,29 @@ answers = []
 Slabels = []
 
 for q in questions:
+    start_time = time.time()
+    
     q_id = loaded_dataset.loc[loaded_dataset['question'] == q, 'id'].values[0]
-    print(f"Processing question#{q_id}:", q)
     
-    answer, softlabels = getSL("Q:"+q+"\nA:")
-    print("Answer:", answer)
+    logger.info(f'Processing question #{q_id}:{q}')
     
-    answers.append(answer)
-    Slabels.append(softlabels)
-    uploader = SoftsUploader(distributions_matrix=Slabels, question_id=q_id)
-    uploader.upload_distributions()
-
-print("Saving into files")
+    try:
+        answer, softlabels = getSL("Q:"+q+"\nA:")
+        processing_time = time.time() - start_time
+        logger.info(f"✅ Answer generated (took {processing_time:.2f}s): {answer}")
+        
+        answers.append(answer)
+        Slabels.append(softlabels)
+        
+        uploader = SoftsUploader(distributions_matrix=Slabels, question_id=q_id, current_time=current_time)
+        uploader.upload_distributions()
+        
+        logger.info(f"Uploaded distributions for question #: {q_id}")
+    except Exception as e:
+        logger.error(f"❌ Error processing question ID {q_id}: {str(e)}", exc_info=True)
+        continue
    
+logger.info("Uploading answers...")
 uploader = AnswersUploader(path=configs.dataset_path, answers=answers)
 uploader.upload_answers()
+logger.info("Uploaded successfully.")
